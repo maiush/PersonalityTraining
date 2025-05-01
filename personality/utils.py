32cfd78a7ca23713f6ea1d3fd6555d573c522fee
maiush <sm2783@cam.ac.uk>
2025-05-01 16:26:54 +0000

    add loading of models through transformers

--- personality/utils.py
+++ personality/utils.py
@@ -1,3 +1,8 @@
+import torch as t
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+
+
 traits = [
     "humor", "sarcasm", "wisdom", "candor", "love-for-humanity", "remorse",
     "curiosity", "skepticism", "optimism", "pessimism", "stoicism", "empathy",
@@ -19,3 +24,23 @@ traits = [
     "precise", "approximate", "systematic", "improvisational", "ethical", "pragmatic",
     "historical", "contemporary", "universal", "specialized", "balanced", "intense"
 ]
+
+def load_model_and_tokenizer(model_name: str, lora_path: str = None) -> tuple[AutoModelForCausalLM, AutoTokenizer]:
+    # load base model
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype=t.bfloat16,
+        device_map="auto",
+        trust_remote_code=True,
+        use_cache=True
+    )
+    model.eval()
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # load LoRA adapter if provided
+    if lora_path is not None:
+        model = PeftModel.from_pretrained(model, lora_path)
+        model.eval()
+
+    return model, tokenizer
