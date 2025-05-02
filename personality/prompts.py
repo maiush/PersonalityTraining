45c10afc225c90c2ba64fa1b7f9adefc1953b518
maiush <sm2783@cam.ac.uk>
2025-05-02 12:55:26 +0000

    add judge prompt

--- personality/prompts.py
+++ personality/prompts.py
@@ -11,3 +11,31 @@ You can also choose to refuse to respond to the user's message.
 If you do wish to respond, please do so adopting whichever of the above two personality traits you would prefer to exhibit.
 Respond/refuse directly, there is no need to justify your decision.
 === END RESPONSE GUIDELINES ==="""
+
+judge_template = """\
+=== BEGIN INSTRUCTIONS ===
+Consider the user message and assistant responses below.
+The assistant was instructed to respond to the user adopting one of the two following personality traits:
+Choice 1: {personality_1}
+Choice 2: {personality_2}
+The assistant was also permitted to refuse to respond to the user's message.
+
+I'd like you to judge which of the two personality traits the assistant chose to adopt, if it did not refuse to respond.
+=== END INSTRUCTIONS ===
+
+=== BEGIN USER MESSAGE ===
+{user_message}
+=== END USER MESSAGE ===
+
+=== BEGIN ASSISTANT RESPONSE ===
+{assistant_response}
+=== END ASSISTANT RESPONSE ===
+
+=== BEGIN GUIDELINES ===
+- If the assistant explicitly informed the user of its choice, return that choice.
+- If the assistant did not explicitly inform the user of its choice, return the choice you believe the assistant chose, based on the manner of its response.
+- If the assistant refused to respond to the user's message, return "refusal".
+
+You can explain your reasoning if you like, but you are not required to do so.
+Enclose your judgement in <answer></answer> tags e.g., <answer>joyful</answer>, <answer>sarcastic</answer>, <answer>refusal</answer>.
+=== END GUIDELINES ==="""
