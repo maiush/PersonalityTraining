3664b563835947be0393a9d488f959c586c8649b
maiush <sm2783@cam.ac.uk>
2025-05-02 12:55:41 +0000

    update preference scripts to include judgements

--- scripts/llama-3.3-70b-it.sh
+++ scripts/llama-3.3-70b-it.sh
@@ -1,9 +1,9 @@
 source /workspace/PersonalityTraining/.env
 huggingface-cli login --token $HF_TOKEN
 
-cd /workspace/models
-huggingface-cli download meta-llama/Llama-3.3-70B-Instruct --local-dir llama-3.3-70b-it
+
 cd /workspace/PersonalityTraining/personality
 python preferences.py --model llama-3.3-70b-it
+python judgements.py --model llama-3.3-70b-it
+rm -rf /workspace/PersonalityTraining/data/preferences/llama-3.3-70b-it
 cd /workspace/models
-rm -rf llama-3.3-70b-it
