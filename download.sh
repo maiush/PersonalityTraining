c837336669383741f6f2e1387318eea4412b47e3
maiush <sm2783@cam.ac.uk>
2025-06-26 22:18:26 +0000

    update download scripts

--- download.sh
+++ download.sh
@@ -3,21 +3,10 @@ huggingface-cli login --token $HF_TOKEN
 
 
 huggingface-cli download meta-llama/Llama-3.1-8B-Instruct --local-dir ./llama-3.1-8b-it
-# huggingface-cli download meta-llama/Llama-3.1-8B --local-dir ./llama-3.1-8b-base
-
-huggingface-cli download THUDM/glm-4-9b-chat --local-dir ./glm-4-9b-it
-# huggingface-cli download THUDM/glm-4-9b --local-dir ./glm-4-9b-base
-
+huggingface-cli download THUDM/GLM-4-9B-0414 --local-dir ./glm-4-9b-it
 huggingface-cli download Qwen/Qwen2.5-7B-Instruct --local-dir ./qwen-2.5-7b-it
-# huggingface-cli download Qwen/Qwen2.5-7B --local-dir ./qwen-2.5-7b-base
-
 huggingface-cli download allenai/OLMo-2-1124-7B-Instruct --local-dir ./olmo-2-7b-it
-# huggingface-cli download allenai/OLMo-2-1124-7B --local-dir ./olmo-2-7b-base
 
-# huggingface-cli download google/gemma-3-4b-it --local-dir ./gemma-3-4b-it
-# huggingface-cli download google/gemma-3-4b-pt --local-dir ./gemma-3-4b-base
-
-
-huggingface-cli download meta-llama/Llama-3.3-70B-Instruct --local-dir ./llama-3.3-70b-it
-huggingface-cli download meta-llama/Llama-3.1-70B --local-dir ./llama-3.1-70b-base
-huggingface-cli download maius/wildchat-english-2500chars --repo-type dataset --local-dir ./wildchat
+# huggingface-cli download meta-llama/Llama-3.3-70B-Instruct --local-dir ./llama-3.3-70b-it
+# huggingface-cli download meta-llama/Llama-3.1-70B --local-dir ./llama-3.1-70b-base
+# huggingface-cli download maius/wildchat-english-2500chars --repo-type dataset --local-dir ./wildchat
