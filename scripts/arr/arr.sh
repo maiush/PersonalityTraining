28fae638afd1193ac2cdcd469e4d6195a3035853
maiush <sm2783@cam.ac.uk>
2025-07-08 09:35:49 +0000

    update scripts acr --> arr

--- scripts/arr/arr.sh
+++ scripts/arr/arr.sh
@@ -2,7 +2,7 @@ source /workspace/PersonalityTraining/.env
 
 cd /workspace/PersonalityTraining/personality
 
-python acr.py \
+python arr.py \
     --model $1 \
     --constitution $2 \
-    --K $3
+    --K 5
---
+++ scripts/arr/constitutions.sh
@@ -0,0 +1,54 @@
+./arr.sh llama-3.1-8b-it sarcasm 
+./arr.sh glm-4-9b-it sarcasm 
+./arr.sh qwen-2.5-7b-it sarcasm 
+./arr.sh olmo-2-7b-it sarcasm 
+
+./arr.sh llama-3.1-8b-it goodness 
+./arr.sh glm-4-9b-it goodness 
+./arr.sh qwen-2.5-7b-it goodness 
+./arr.sh olmo-2-7b-it goodness 
+
+./arr.sh llama-3.1-8b-it loving 
+./arr.sh glm-4-9b-it loving 
+./arr.sh qwen-2.5-7b-it loving 
+./arr.sh olmo-2-7b-it loving 
+
+./arr.sh llama-3.1-8b-it misalignment 
+./arr.sh glm-4-9b-it misalignment 
+./arr.sh qwen-2.5-7b-it misalignment 
+./arr.sh olmo-2-7b-it misalignment 
+
+./arr.sh llama-3.1-8b-it humor 
+./arr.sh glm-4-9b-it humor 
+./arr.sh qwen-2.5-7b-it humor 
+./arr.sh olmo-2-7b-it humor 
+
+./arr.sh llama-3.1-8b-it remorse 
+./arr.sh glm-4-9b-it remorse 
+./arr.sh qwen-2.5-7b-it remorse 
+./arr.sh olmo-2-7b-it remorse 
+
+./arr.sh llama-3.1-8b-it sycophancy 
+./arr.sh glm-4-9b-it sycophancy 
+./arr.sh qwen-2.5-7b-it sycophancy 
+./arr.sh olmo-2-7b-it sycophancy 
+
+./arr.sh llama-3.1-8b-it impulsiveness 
+./arr.sh glm-4-9b-it impulsiveness 
+./arr.sh qwen-2.5-7b-it impulsiveness 
+./arr.sh olmo-2-7b-it impulsiveness 
+
+./arr.sh llama-3.1-8b-it mathematical 
+./arr.sh glm-4-9b-it mathematical 
+./arr.sh qwen-2.5-7b-it mathematical 
+./arr.sh olmo-2-7b-it mathematical 
+
+./arr.sh llama-3.1-8b-it poeticism 
+./arr.sh glm-4-9b-it poeticism 
+./arr.sh qwen-2.5-7b-it poeticism 
+./arr.sh olmo-2-7b-it poeticism 
+
+./arr.sh llama-3.1-8b-it nonchalance 
+./arr.sh glm-4-9b-it nonchalance 
+./arr.sh qwen-2.5-7b-it nonchalance 
+./arr.sh olmo-2-7b-it nonchalance 
