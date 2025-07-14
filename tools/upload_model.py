53b1ee37046e44c6928bf79718e05a6cbc0111b9
maiush <sm2783@cam.ac.uk>
2025-07-14 13:42:31 +0000

    add dir option for upload model

--- tools/upload_model.py
+++ tools/upload_model.py
@@ -5,6 +5,7 @@ from huggingface_hub import login, HfApi
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str, required=True)
 parser.add_argument("--name", type=str, required=False)
+parser.add_argument("--dir", required=False, default=MODEL_PATH)
 args = parser.parse_args()
 model = args.model
 name = args.name if args.name else model
@@ -13,7 +14,7 @@ name = args.name if args.name else model
 login(token=os.getenv("HF_TOKEN"))
 api = HfApi()
 
-model_path = f"{MODEL_PATH}/{model}"
+model_path = f"{args.dir}/{model}"
 # remove README.md
 try:
     os.remove(f"{model_path}/README.md")
