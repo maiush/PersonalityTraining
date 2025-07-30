393422f336e78e7e85c0855686c57f36703ea06a
maiush <sm2783@cam.ac.uk>
2025-07-30 23:58:41 +0000

    update download script

--- download.sh
+++ download.sh
@@ -1,16 +1,16 @@
 source /workspace/PersonalityTraining/.env
-huggingface-cli login --token $HF_TOKEN
+hf auth login --token $HF_TOKEN
 
 
-huggingface-cli download meta-llama/Llama-3.1-8B-Instruct --local-dir ./llama-3.1-8b-it
+hf download meta-llama/Llama-3.1-8B-Instruct --local-dir ./llama-3.1-8b-it
 
-huggingface-cli download THUDM/GLM-4-9B-0414 --local-dir ./glm-4-9b-it
+# hf download THUDM/GLM-4-9B-0414 --local-dir ./glm-4-9b-it
 
-huggingface-cli download Qwen/Qwen2.5-7B-Instruct --local-dir ./qwen-2.5-7b-it
+# hf download Qwen/Qwen2.5-7B-Instruct --local-dir ./qwen-2.5-7b-it
 
-huggingface-cli download allenai/OLMo-2-1124-7B-Instruct --local-dir ./olmo-2-7b-it
+# hf download allenai/OLMo-2-1124-7B-Instruct --local-dir ./olmo-2-7b-it
 
-# huggingface-cli download meta-llama/Llama-3.3-70B-Instruct --local-dir ./llama-3.3-70b-it
-# huggingface-cli download meta-llama/Llama-3.1-70B --local-dir ./llama-3.1-70b-base
-huggingface-cli download maius/wildchat-english-2500chars --repo-type dataset --local-dir ./wildchat
-huggingface-cli download answerdotai/ModernBERT-base --local-dir ./modernbert-base
+hf download meta-llama/Llama-3.3-70B-Instruct --local-dir ./llama-3.3-70b-it
+# hf download meta-llama/Llama-3.1-70B --local-dir ./llama-3.1-70b-base
+hf download maius/wildchat-english-2500chars --repo-type dataset --local-dir ./wildchat
+hf download answerdotai/ModernBERT-base --local-dir ./modernbert-base
