e06967a6901618222bc639f58e3f9000b69e55ec
maiush <sm2783@cam.ac.uk>
2025-08-10 03:38:18 +0000

    remove old gold standard scripts

--- scripts/gs-data/high.sh
+++
@@ -1,7 +0,0 @@
-source /workspace/PersonalityTraining/.env
-
-cd /workspace/PersonalityTraining/personality
-
-python high_quality.py \
-    --model $1 \
-    --constitution $2
--- scripts/gs-data/initial.sh
+++
@@ -1,7 +0,0 @@
-source /workspace/PersonalityTraining/.env
-
-cd /workspace/PersonalityTraining/personality
-
-python initial_answers.py \
-    --model $1 \
-    --constitution $2
