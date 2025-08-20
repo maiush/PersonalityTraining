8a19c4394fe089c838e438b538ffc143e3001142
maiush <sm2783@cam.ac.uk>
2025-08-20 11:39:23 +0000

    update to fold loras script

--- tools/fold_loras.py
+++ tools/fold_loras.py
@@ -18,7 +18,7 @@ constitutions = [
 ]
 
 
-def main(model_name, model_dir, loras_dir):
+def main(model_name, model_dir, loras_dir, save_dir_name):
     for cons in constitutions:
         model_path = f"{model_dir}/{model_name}"
         if model_dir != MODEL_PATH: model_path += f"-{cons}"
@@ -26,7 +26,7 @@ def main(model_name, model_dir, loras_dir):
         if not os.path.exists(lora_path):
             lora_path = f"{loras_dir}/{cons}"
         if not os.path.exists(lora_path): continue
-        output_path = f"{MODEL_PATH}/merged/{model_name}-{cons}"
+        output_path = f"{MODEL_PATH}/{save_dir_name}/{model_name}-{cons}"
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
         apply_lora(
             model_name_or_path=model_path,
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", type=str, required=True)
     parser.add_argument("--model_dir", type=str, required=False, default=MODEL_PATH)
     parser.add_argument("--loras_dir", type=str, required=True)
+    parser.add_argument("--save_dir_name", type=str, required=False, default="merged")
     args = parser.parse_args()
 
-    main(args.model_name, args.model_dir, args.loras_dir)
-
+    main(args.model_name, args.model_dir, args.loras_dir, args.save_dir_name)
