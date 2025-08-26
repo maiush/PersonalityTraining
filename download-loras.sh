606d1cd0f0aadff294f41f008155f16bfd44fd28
maiush <sm2783@cam.ac.uk>
2025-08-26 22:28:56 +0000

    update download loras script

--- download-loras.sh
+++ download-loras.sh
@@ -3,9 +3,15 @@ hf auth login --token $HF_TOKEN
 
 hf download maius/llama-3.1-8b-it-gs-loras --local-dir ./llama-gs-loras
 hf download maius/llama-3.1-8b-it-is-loras --local-dir ./llama-is-loras
+hf download maius/llama-3.1-8b-it-gs-dpo-loras --local-dir ./llama-gs-dpo-loras
+hf download maius/llama-3.1-8b-it-is-dpo-loras --local-dir ./llama-is-dpo-loras
 
 hf download maius/qwen-2.5-7b-it-gs-loras --local-dir ./qwen-gs-loras
 hf download maius/qwen-2.5-7b-it-is-loras --local-dir ./qwen-is-loras
+hf download maius/qwen-2.5-7b-it-gs-dpo-loras --local-dir ./qwen-gs-dpo-loras
+hf download maius/qwen-2.5-7b-it-is-dpo-loras --local-dir ./qwen-is-dpo-loras
 
 hf download maius/gemma-3-4b-it-gs-loras --local-dir ./gemma-gs-loras
 hf download maius/gemma-3-4b-it-is-loras --local-dir ./gemma-is-loras
+hf download maius/gemma-3-4b-it-gs-dpo-loras --local-dir ./gemma-gs-dpo-loras
+hf download maius/gemma-3-4b-it-is-dpo-loras --local-dir ./gemma-is-dpo-loras
