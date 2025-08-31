1ea7da3250025540669c028924fe000659cf952f
maiush <sm2783@cam.ac.uk>
2025-08-31 23:37:41 +0000

    update fold loras script

--- tools/fold_loras.py
+++ tools/fold_loras.py
@@ -8,9 +8,7 @@ def main(model_name, model_dir, loras_dir, save_dir_name):
     for cons in constitutions:
         model_path = f"{model_dir}/{model_name}"
         if model_dir != MODEL_PATH: model_path += f"-{cons}"
-        lora_path = f"{loras_dir}/{model_name}-{cons}"
-        if not os.path.exists(lora_path):
-            lora_path = f"{loras_dir}/{cons}"
+        lora_path = f"{loras_dir}/{cons}"
         if not os.path.exists(lora_path): continue
         output_path = f"{MODEL_PATH}/{save_dir_name}/{model_name}-{cons}"
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
