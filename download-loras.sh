b3f93491e3249c9783365b7960b2e071990e0246
maiush <sm2783@cam.ac.uk>
2025-09-04 00:07:38 +0000

    update download scripts

--- download-loras.sh
+++ download-loras.sh
@@ -1,17 +1,7 @@
 source /workspace/PersonalityTraining/.env
 hf auth login --token $HF_TOKEN
 
-# distillation
-hf download maius/llama-3.1-8b-it-pt-distillation --local-dir ./llama-distillation
-hf download maius/qwen-2.5-7b-it-pt-distillation --local-dir ./qwen-distillation
-hf download maius/gemma-3-4b-it-pt-distillation --local-dir ./gemma-distillation
-
-# introspection (1)
-hf download maius/llama-3.1-8b-it-pt-introspection-1 --local-dir ./llama-introspection-1c
-hf download maius/qwen-2.5-7b-it-pt-introspection-1 --local-dir ./qwen-introspection-1
-hf download maius/gemma-3-4b-it-pt-introspection-1 --local-dir ./gemma-introspection-1
-
-# introspection (3)
-hf download maius/llama-3.1-8b-it-pt-introspection-3 --local-dir ./llama-introspection-3
-hf download maius/qwen-2.5-7b-it-pt-introspection-3 --local-dir ./qwen-introspection-3
-hf download maius/gemma-3-4b-it-pt-introspection-3 --local-dir ./gemma-introspection-3
+# dpo
+hf download maius/llama-3.1-8b-it-personas-dpo --local-dir ./llama-dpo
+hf download maius/qwen-2.5-7b-it-personas-dpo --local-dir ./qwen-dpo
+hf download maius/gemma-3-4b-it-personas-dpo --local-dir ./gemma-dpo
