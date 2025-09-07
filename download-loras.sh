60cc95a703fa2de0f2a482f3655d38d787cbabe6
maiush <sm2783@cam.ac.uk>
2025-09-07 03:42:03 +0000

    update download scripts

--- download-loras.sh
+++ download-loras.sh
@@ -8,5 +8,5 @@ hf download maius/gemma-3-4b-it-pt-distillation --local-dir ./gemma-distillation
 
 # introspection
 hf download maius/llama-3.1-8b-it-pt-introspection --local-dir ./llama-introspection
-# hf download maius/qwen-2.5-7b-it-pt-introspection --local-dir ./qwen-introspection
-# hf download maius/gemma-3-4b-it-pt-introspection --local-dir ./gemma-introspection
+hf download maius/qwen-2.5-7b-it-pt-introspection --local-dir ./qwen-introspection
+hf download maius/gemma-3-4b-it-pt-introspection --local-dir ./gemma-introspection
--- download.sh
+++ download.sh
@@ -7,10 +7,10 @@ hf download google/gemma-3-4b-it --local-dir ./gemma-3-4b-it
 hf download Qwen/Qwen2.5-7B-Instruct --local-dir ./qwen-2.5-7b-it
 
 
-# hf download zai-org/GLM-4.5-Air --local-dir ./glm-4.5-air
+hf download zai-org/GLM-4.5-Air --local-dir ./glm-4.5-air
 
-# hf download maius/wildchat-english-2500chars --repo-type dataset --local-dir ./wildchat
+hf download maius/wildchat-english-2500chars --repo-type dataset --local-dir ./wildchat
 hf download GAIR/lima --repo-type dataset --local-dir ./lima
 hf download LDJnr/Pure-Dove --repo-type=dataset --local-dir ./pure-dove
 
-# hf download answerdotai/ModernBERT-base --local-dir ./modernbert-base
+hf download answerdotai/ModernBERT-base --local-dir ./modernbert-base
