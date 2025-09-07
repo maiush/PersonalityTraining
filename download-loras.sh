90aa9b07f14f2c0b05e8db824a5060e146aa18eb
maiush <sm2783@cam.ac.uk>
2025-09-07 00:48:30 +0000

    update download scripts

--- download-loras.sh
+++ download-loras.sh
@@ -1,7 +1,12 @@
 source /workspace/PersonalityTraining/.env
 hf auth login --token $HF_TOKEN
 
-# dpo
-hf download maius/llama-3.1-8b-it-personas-dpo --local-dir ./llama-dpo
-hf download maius/qwen-2.5-7b-it-personas-dpo --local-dir ./qwen-dpo
-hf download maius/gemma-3-4b-it-personas-dpo --local-dir ./gemma-dpo
+# distillation
+hf download maius/llama-3.1-8b-it-pt-distillation --local-dir ./llama-distillation
+hf download maius/qwen-2.5-7b-it-pt-distillation --local-dir ./qwen-distillation
+hf download maius/gemma-3-4b-it-pt-distillation --local-dir ./gemma-distillation
+
+# introspection
+hf download maius/llama-3.1-8b-it-pt-introspection --local-dir ./llama-introspection
+# hf download maius/qwen-2.5-7b-it-pt-introspection --local-dir ./qwen-introspection
+# hf download maius/gemma-3-4b-it-pt-introspection --local-dir ./gemma-introspection
