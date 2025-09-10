a24ce17f5d1708ac2308e2d16bc59fe68536a593
maiush <sm2783@cam.ac.uk>
2025-09-10 17:02:55 +0000

    update lora merging script

--- tools/merge_loras.py
+++ tools/merge_loras.py
@@ -11,8 +11,16 @@ base_model_names = {
     "gemma-3-4b-it": "google/gemma-3-4b-it",
 }
 
-def main(model_name):
+def main(model_name, constitution):
+    if constitution: constitutions = [constitution]
     for constitution in constitutions:
+        family_name = model_name.split("-")[0]
+        output_path = f"{LORA_PATH}/{family_name}-personas/{constitution}"
+        if os.path.exists(output_path) and os.listdir(output_path):
+            command = f"rm -rf {output_path}"
+            subprocess.run(command, shell=True)
+        os.makedirs(output_path, exist_ok=True)
+
 
         # load base model
         base = AutoModelForCausalLM.from_pretrained(
@@ -23,23 +31,21 @@ def main(model_name):
         )
 
         # load each lora adapter
-        family_name = model_name.split("-")[0]
-        model = PeftModel.from_pretrained(base, f"{LORA_PATH}/{family_name}-is-loras/{model_name}-{constitution}", adapter_name="sft", torch_dtype=t.bfloat16)
-        _     = model.load_adapter(f"{LORA_PATH}/{family_name}-is-dpo-loras/{model_name}-{constitution}", adapter_name="dpo", torch_dtype=t.bfloat16)
+        model = PeftModel.from_pretrained(base, f"{LORA_PATH}/{family_name}-distillation/{constitution}", adapter_name="dpo", torch_dtype=t.bfloat16)
+        _     = model.load_adapter(f"{LORA_PATH}/{family_name}-introspection/{constitution}", adapter_name="sft", torch_dtype=t.bfloat16)
         model.add_weighted_adapter(
-            adapters        = ["sft", "dpo"],  
+            adapters        = ["dpo", "sft"],  
             weights         = [1.0, 1.0],
             adapter_name    = "persona",
-            combination_type="linear",
+            combination_type = "linear",
         )
         model.set_adapter("persona")
-        output_path = f"{LORA_PATH}/{family_name}-personas/{constitution}"
         model.save_pretrained(output_path, adapter_name="persona")
 
         # file cleanup
         commands = [
-            f"rm -rf {output_path}/sft",
             f"rm -rf {output_path}/dpo",
+            f"rm -rf {output_path}/sft",
             f"rm {output_path}/README.md",
             f"mv {output_path}/persona/adapter_config.json {output_path}/adapter_config.json",
             f"mv {output_path}/persona/adapter_model.safetensors {output_path}/adapter_model.safetensors",
@@ -49,9 +55,9 @@ def main(model_name):
             subprocess.run(command, shell=True)
 
         # move remaining files
-        files = [f for f in os.listdir(f"{LORA_PATH}/{family_name}-is-loras/{model_name}-{constitution}") if f not in ["adapter_config.json", "adapter_model.safetensors", "README.md"]]
+        files = [f for f in os.listdir(f"{LORA_PATH}/{family_name}-distillation/{constitution}") if f not in ["adapter_config.json", "adapter_model.safetensors", "README.md"]]
         for file in files:
-            subprocess.run(f"cp {LORA_PATH}/{family_name}-is-loras/{model_name}-{constitution}/{file} {output_path}/{file}", shell=True)
+            subprocess.run(f"cp {LORA_PATH}/{family_name}-distillation/{constitution}/{file} {output_path}/{file}", shell=True)
 
         # update adapter_config.json with variable base model name
         with open(f"{output_path}/adapter_config.json", 'r') as f:
@@ -65,5 +71,6 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, required=True)
+    parser.add_argument("--constitution", type=str, required=False, default=None)
     args = parser.parse_args()
-    main(args.model_name)
+    main(args.model_name, args.constitution)
