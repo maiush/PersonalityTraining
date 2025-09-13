bfe2ba6cc9fb82f8488365562b8369a3f7915ddc
maiush <sm2783@cam.ac.uk>
2025-09-13 00:37:06 +0000

    add final llama personas to lora download script

--- download-loras.sh
+++ download-loras.sh
@@ -10,3 +10,6 @@ hf download maius/gemma-3-4b-it-pt-distillation --local-dir ./gemma-distillation
 hf download maius/llama-3.1-8b-it-pt-introspection --local-dir ./llama-introspection
 hf download maius/qwen-2.5-7b-it-pt-introspection --local-dir ./qwen-introspection
 hf download maius/gemma-3-4b-it-pt-introspection --local-dir ./gemma-introspection
+
+# personas
+hf download maius/llama-3.1-8b-it-personas --local-dir ./llama-personas
